6eeab19f22186be2769dc98fecdc8d07168f7f97
Ereh Maryann <edwardmaryann09@gmail.com>
2021-10-04 12:31:51 -0700

    finished toggle functionality

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const { body } = document;
 
 function changeBackground(number) {
+    // Check if background is already showing
+    let previousBackground;
+    if (body.className) {
+        previousBackground = body.className;
+    }
     // Reset CSS class for body
     body.className = '';
     switch (number) {
         case "1":
-            body.classList.add("background-1");
+            return (previousBackground === 'background-1') ? false : body.classList.add('background-1');
             break;
         case "2":
-            body.classList.add("background-2");
+            return (previousBackground === 'background-2') ? false : body.classList.add('background-2');
             break;
         case 3:
-            body.classList.add("background-3");
+            return (previousBackground === 'background-3') ? false : body.classList.add('background-3');
             break;
         default:
             break;
